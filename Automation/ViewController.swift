cdb9e41138faf7e6dd6a435fde0b0d7a309bf7a3
Teodor Garzdin <tsg@hugelawn.com>
2016-11-30 14:59:55 +0100

    Import files.

--- Automation/ViewController.swift
+++ Automation/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var humidityLabel: UILabel!
+    @IBOutlet var lightLabel: UILabel!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        spinner.hidesWhenStopped = true
+        getData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func updateAction(_ sender: Any) {
+        getData()
+    }
+    
+    func getData() {
+        spinner.startAnimating()
+        Alamofire.request("https://automationapi.herokuapp.com/api/retrieve").responseJSON { response in
+            if let data = response.result.value as? [Any] {
+                if let lastEntry = data.last as? [String:Any] {
+                    if let temperature = lastEntry["temperature"] as? Int,
+                        let humidity = lastEntry["humidity"] as? Int,
+                        let light = lastEntry["light"] as? Int {
+                        DispatchQueue.main.async {
+                            self.temperatureLabel.text = "Temperature: \(temperature) C"
+                            self.humidityLabel.text = "Humidity: \(humidity) %"
+                            self.lightLabel.text = "Light: \(light) lm"
+                        }
+                    }
+                }
+            }
+        }
+        spinner.stopAnimating()
+    }
 }
 
